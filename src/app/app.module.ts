58b56e83375c64a87011aadf7c4f074d7c091215
Aditya Yadav <adityayadav135952@gmail.com>
2025-08-26 12:37:11 +0530

    fontawesome installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SellerHomeComponent } from './Components/seller-home/seller-home.component';
 import { SellerAddProductComponent } from './Components/seller-add-product/seller-add-product.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { SellerAddProductComponent } from './Components/seller-add-product/selle
     BrowserModule,
     AppRoutingModule,
     FormsModule, 
-    HttpClientModule
+    HttpClientModule, FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
